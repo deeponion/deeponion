42fc293159ba6ef5cd094947ddf1a89837aa8eee
Benedict Chan <bencxr@fragnetics.com>
2014-09-07 12:37:13 +0000

    update rpc help message for gettransaction to add includeWatchonly param
    Rebased-from: 57e1716de6a6deba23ad8c4152cc0a32a4974c41

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -1484,7 +1484,7 @@ Value gettransaction(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() < 1 || params.size() > 2)
         throw runtime_error(
-            "gettransaction \"txid\"\n"
+            "gettransaction \"txid\" ( includeWatchonly )\n"
             "\nGet detailed information about in-wallet transaction <txid>\n"
             "\nArguments:\n"
             "1. \"txid\"    (string, required) The transaction id\n"
@@ -1513,6 +1513,7 @@ Value gettransaction(const Array& params, bool fHelp)
 
             "\nbExamples\n"
             + HelpExampleCli("gettransaction", "\"1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d\"")
+            + HelpExampleCli("gettransaction", "\"1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d\" true")
             + HelpExampleRpc("gettransaction", "\"1075db55d416d3ca199f55b6084e2115b9345e16c5cf302fc80e9d5fbf5d48d\"")
         );
 
