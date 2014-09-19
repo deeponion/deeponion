480f0e7af20054f902509518edacb41eb63926d6
Warren Togami <wtogami@gmail.com>
2014-09-19 00:16:33 -1000

    Litecoin: Silence two non-errors during ProcessMessage disconnect
    Otherwise it spams the log with "ProcessMessage(version, 106 bytes) FAILED" for no good reason.

--- src/main.cpp
+++ src/main.cpp
@@ -3492,7 +3492,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
             pfrom->PushMessage("reject", strCommand, REJECT_OBSOLETE,
                                strprintf("Version must be %d or greater", MIN_PEER_PROTO_VERSION));
             pfrom->fDisconnect = true;
-            return false;
+            return true;
         }
 
         if (pfrom->nVersion == 10300)
@@ -3513,7 +3513,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
                 LogPrintf("invalid subver %s at %s, disconnecting\n", pfrom->cleanSubVer, pfrom->addr.ToString());
                 pfrom->PushMessage("reject", strCommand, REJECT_INVALID, string("invalid client subver"));
                 pfrom->fDisconnect = true;
-                return false;
+                return true;
             }
         }
 
