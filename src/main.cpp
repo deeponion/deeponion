39792671807d2188552118f9e265ee3d2fec3a93
Luke Dashjr <luke-jr+git@utopios.org>
2015-04-09 11:50:18 +0000

    Bugfix: Allow mining on top of old tip blocks for testnet (fixes testnet-in-a-box use case)

--- src/main.cpp
+++ src/main.cpp
@@ -1257,6 +1257,7 @@ CAmount GetBlockValue(int nHeight, const CAmount& nFees)
 
 bool IsInitialBlockDownload()
 {
+    const CChainParams& chainParams = Params();
     LOCK(cs_main);
     if (fImporting || fReindex || chainActive.Height() < Checkpoints::GetTotalBlocksEstimate())
         return true;
@@ -1264,7 +1265,7 @@ bool IsInitialBlockDownload()
     if (lockIBDState)
         return false;
     bool state = (chainActive.Height() < pindexBestHeader->nHeight - 24 * 6 ||
-            pindexBestHeader->GetBlockTime() < GetTime() - 24 * 60 * 60);
+            pindexBestHeader->GetBlockTime() < GetTime() - chainParams.MaxTipAge());
     if (!state)
         lockIBDState = true;
     return state;
