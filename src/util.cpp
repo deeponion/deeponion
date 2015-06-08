960097a5ef95a974680521754c7af85fe3b2f202
Gitju <juergen@scholz-email.de>
2015-06-08 18:10:38 +0200

    Pid file is litecoind.pid

--- src/util.cpp
+++ src/util.cpp
@@ -477,7 +477,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 #ifndef WIN32
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "litecoin.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "litecoind.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
