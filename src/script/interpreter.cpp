da6842903cd1f61671dc43ca38347c69df8a792f
aitorp6 <aitorp@gmail.com>
2018-09-03 10:37:11 +0200

    Removed some warning messages

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1424,7 +1424,6 @@ static bool VerifyWitnessProgram(const CScriptWitness& witness, int witversion, 
 
 bool VerifyScript(const CScript& scriptSig, const CScript& scriptPubKey, const CScriptWitness* witness, unsigned int flags, const BaseSignatureChecker& checker, ScriptError* serror)
 {
-	const CTransaction* tx = ((TransactionSignatureChecker&)checker).txTo;
 	
     static const CScriptWitness emptyWitness;
     if (witness == nullptr) {
