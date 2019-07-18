b03c54caa59249e88986e09caa1787964507ce46
dragonvslinux <35779158+dragononcrypto@users.noreply.github.com>
2019-07-18 08:30:55 +0200

    Update linux.sh
    Update nodes script

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -3,6 +3,6 @@ cd ~/.DeepOnion
 mv -f DeepOnion.conf .DeepOnion.conf.bak
 sudo apt-get install curl -y
 echo "$(curl https://deeponion.org/conf_file/DeepOnion.conf)" >> ~/.DeepOnion/DeepOnion.conf
-echo "staking=1" >> ~/.DeepOnion/DeepOnion.conf
-echo "checkblockchain=0" >> ~/.DeepOnion/DeepOnion.conf
-echo Task finished! Restart the DeepOnion wallet.
+echo "staking=1
+checkblockchain=0" >> ~/.DeepOnion/DeepOnion.conf
+echo Nodes finished! Restart the DeepOnion wallet.
