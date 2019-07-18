269f05808885066cac548aa023100c34864fac86
dragonvslinux <35779158+dragononcrypto@users.noreply.github.com>
2019-07-18 21:02:10 +0200

    Update linux.sh
    Added ca-certificate for wget command, tested on debian and ubuntu.

--- contrib/scripts/linux.sh
+++ contrib/scripts/linux.sh
@@ -1,8 +1,7 @@
 mkdir -p ~/.DeepOnion
 cd ~/.DeepOnion
 mv -f DeepOnion.conf .DeepOnion.conf.bak
-sudo apt-get install curl -y
-echo "$(curl https://deeponion.org/conf_file/DeepOnion.conf)" >> ~/.DeepOnion/DeepOnion.conf
+wget https://deeponion.org/conf_file/DeepOnion.conf --ca-certificate=https://raw.githubusercontent.com/deeponion/deeponion/tree/master/contrib/scripts/deeponion-cert.txt
 echo "staking=1
 checkblockchain=0" >> ~/.DeepOnion/DeepOnion.conf
 echo Nodes updated! Restart the DeepOnion wallet.
