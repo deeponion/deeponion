9208bfb50e6b5dc88dbe2c6e610678b2ebddc854
dragonvslinux <35779158+dragononcrypto@users.noreply.github.com>
2019-07-21 09:12:52 +0200

    Update linux.sh
    echo downloading actions for transparency

--- contrib/scripts/linux.sh
+++ contrib/scripts/linux.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.DeepOnion
 cd ~/.DeepOnion
-mv -f DeepOnion.conf .DeepOnion.conf.bak
+mv -f DeepOnion.conf .DeepOnion.conf.bak && echo Downloading DeepOnion.org SSL Certificate...
 echo | openssl s_client -servername deeponion.org -connect deeponion.org:443 |\
-  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ~/.DeepOnion/deeponion.crt
+  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > ~/.DeepOnion/deeponion.crt && echo Downloading node configuration file...
 wget https://deeponion.org/conf_file/DeepOnion.conf --ca-certificate=~/.DeepOnion/deeponion.crt
 echo "staking=1
 checkblockchain=0" >> ~/.DeepOnion/DeepOnion.conf
