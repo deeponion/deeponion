f5b31c4f3cea7695285d8d745c32e2b4e0e75d00
thohemp <thorstenhempel@web.de>
2019-12-22 19:50:59 +0100

    update quicksync source

--- src/qt/modaloverlay.h
+++ src/qt/modaloverlay.h
@@ -98,7 +98,7 @@ private:
     bool deflationrequested;
     Downloader m_downloader;
     GUIUtil::QuickSync quickS;
-    QUrl blockchain_url = QString("http://45.77.201.153/blockchain_rebased.tar.gz");
+    QUrl blockchain_url = QString("http://80.240.30.125/blockchain_rebased.tar.gz");
     QString getQuickSyncStatus();
     QuickSyncStatus quickSyncStatus;
     fs::path tempquickSyncDir;
