824407d367ea36ea5755b81a02c453ab9085d8e9
thohemp <thorstenhempel@web.de>
2019-12-22 20:35:44 +0100

    fix race condition: https://github.com/bitcoin/bitcoin/pull/13212

--- src/net.cpp
+++ src/net.cpp
@@ -1162,6 +1162,17 @@ void CConnman::ThreadSocketHandler()
         //
         {
             LOCK(cs_vNodes);
+
+            if (!fNetworkActive) {
+                // Disconnect any connected nodes
+                for (CNode* pnode : vNodes) {
+                    if (!pnode->fDisconnect) {
+                        LogPrint(BCLog::NET, "Network not active, dropping peer=%d\n", pnode->GetId());
+                        pnode->fDisconnect = true;
+                    }
+                }
+            }
+
             // Disconnect unused nodes
             std::vector<CNode*> vNodesCopy = vNodes;
             for (CNode* pnode : vNodesCopy)
@@ -2203,16 +2214,6 @@ void CConnman::SetNetworkActive(bool active)
 
     if (fNetworkActive == active) {
         return;
-    }
-
-    fNetworkActive = active;
-
-    if (!fNetworkActive) {
-        LOCK(cs_vNodes);
-        // Close sockets to all nodes
-        for (CNode* pnode : vNodes) {
-            pnode->CloseSocketDisconnect();
-        }
     }
 
     uiInterface.NotifyNetworkActiveChanged(fNetworkActive);
