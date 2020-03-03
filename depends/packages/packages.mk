b96ab95c8a2bba660e54bcc7a82c724db3cce0c3
nezero <nezero@nezero.co.uk>
2020-03-03 15:15:20 +0000

    Move Linux Tor depends away from QT

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -4,8 +4,10 @@ native_packages := native_ccache
 qt_native_packages = native_protobuf
 qt_packages = qrencode protobuf zlib
 
-qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans libseccomp libcap attr
-#qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
+linux_packages:=libseccomp libcap attr
+
+qt_x86_64_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libX11 xextproto libXext xtrans
+qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 
 qt_darwin_packages=qt
