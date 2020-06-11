e4fe2c52aa286f480287d8efc83903060738cb13
Liam Alford <nezero@users.noreply.github.com>
2020-06-11 14:26:04 +0100

    libdb5.3++-dev
    Add flags for incompatible libdb

--- ci/test/00_setup_env_native.sh
+++ ci/test/00_setup_env_native.sh
@@ -8,10 +8,10 @@
 
 export LC_ALL=C.UTF-8
 
-export PACKAGES="python3-zmq qtbase5-dev qttools5-dev-tools libdbus-1-dev libharfbuzz-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libzmq3-dev libseccomp-dev libcap-dev libattr1-dev libdb-dev gettext"
+export PACKAGES="python3-zmq qtbase5-dev qttools5-dev-tools libdbus-1-dev libharfbuzz-dev build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils python3 libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libzmq3-dev libseccomp-dev libcap-dev libattr1-dev libdb5.3++-dev gettext"
 export DEP_OPTS="NO_QT=1 NO_UPNP=1 DEBUG=1 ALLOW_HOST_PACKAGES=1"
 export RUN_UNIT_TESTS=false
 export NO_DEPENDS=1
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="install"
-export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --enable-glibc-back-compat --enable-reduce-exports --enable-debug"
+export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --with-incompatible-bdb --enable-glibc-back-compat --enable-reduce-exports --enable-debug"
