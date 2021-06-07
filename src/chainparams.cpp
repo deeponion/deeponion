a74add19fefbbf035c01803b08763f5ade711248
nezero <nezero@nezero.co.uk>
2021-06-07 12:03:03 +0100

    Push back CSV fork.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -327,8 +327,8 @@ public:
 
         // Deployment of BIP68, BIP112, and BIP113.
         consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1618657200; // Sat Apr 17 2021 11:00:00 GMT+0000
-        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1621249200; // Mon May 17 2021 11:00:00 GMT+0000
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 1625140800; // Jul 01 2021 12:00:00 GMT+0000
+        consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 1627819200; // Aug 01 2021 12:00:00 GMT+0000
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
