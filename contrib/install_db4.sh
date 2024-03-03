4c952a844017125cb88727eae906362822e09993
Harris Brakmić <brakmic@users.noreply.github.com>
2024-03-03 17:06:00 +0100

    Win32 build fixes and Tor 0.4.8 update (#85)
    * feat: update tor to 0.4.8
    
    * fix: tor compilation
    
    * fix: bdb4 compilation with install script
    
    * fix: bdb4 compilation with depends
    
    * fix: use gitlab tor repository
    
    * chore: move tor directory to root

--- contrib/install_db4.sh
+++ contrib/install_db4.sh
@@ -63,6 +63,11 @@ http_get "${BDB_URL}" "${BDB_VERSION}.tar.gz" "${BDB_HASH}"
 tar -xzvf ${BDB_VERSION}.tar.gz -C "$BDB_PREFIX"
 cd "${BDB_PREFIX}/${BDB_VERSION}/"
 
+chmod a+w dbinc/atomic.h
+chmod a+w mp/mp_mvcc.c
+chmod a+w mp/mp_fget.c
+chmod a+w mp/mp_region.c
+
 # Apply a patch necessary when building with clang and c++11 (see https://community.oracle.com/thread/3952592)
 CLANG_CXX11_PATCH_URL='https://gist.githubusercontent.com/LnL7/5153b251fd525fe15de69b67e63a6075/raw/7778e9364679093a32dec2908656738e16b6bdcb/clang.patch'
 CLANG_CXX11_PATCH_HASH='7a9a47b03fd5fb93a16ef42235fa9512db9b0829cfc3bdf90edd3ec1f44d637c'
